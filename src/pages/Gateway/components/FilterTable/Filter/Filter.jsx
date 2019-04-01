5c679dcf4cd4763a7cb4fa8c8b99b532401cefb4
xwluan <xwluan@dfocuspace.com>
2019-04-01 11:29:07 +0800

    网关页面，修改保存网关信息

--- src/pages/Gateway/components/FilterTable/Filter/Filter.jsx
+++ src/pages/Gateway/components/FilterTable/Filter/Filter.jsx
@@ -22,25 +22,31 @@ export default class Filter extends Component {
         <div>
           <Row wrap>
             <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
-              <label style={styles.filterTitle}>所属应用</label>
-              <IceFormBinder name="app">
-                <Input />
+              <label style={styles.filterTitle}>网关名</label>
+              <IceFormBinder name="gwname">
+                <Input hasClear placeholder="eg: luan" />
               </IceFormBinder>
             </Col>
             <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
-              <label style={styles.filterTitle}>分类ID</label>
-              <IceFormBinder name="id">
-                <Input />
+              <label style={styles.filterTitle}>网关种类</label>
+              <IceFormBinder name="gwtype">
+                <Input hasClear placeholder="eg: test" />
               </IceFormBinder>
             </Col>
             <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
-              <label style={styles.filterTitle}>标签ID</label>
-              <IceFormBinder name="tag">
-                <Input />
+              <label style={styles.filterTitle}>网关别名</label>
+              <IceFormBinder name="gwAlias">
+                <Input hasClear placeholder="eg: 畅梦科技有限公司" />
               </IceFormBinder>
             </Col>
             <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
-              <label style={styles.filterTitle}>开始时间</label>
+              <label style={styles.filterTitle}>网关地址</label>
+              <IceFormBinder name="gwIp">
+                <Input  hasClear placeholder="eg: http://127.0.0.1"/>
+              </IceFormBinder>
+            </Col>
+            {/* <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
+              <label style={styles.filterTitle}>网关地址</label>
               <IceFormBinder
                 name="startTime"
                 valueFormatter={(date, strValue) => {
@@ -49,8 +55,39 @@ export default class Filter extends Component {
               >
                 <DatePicker style={styles.filterTool} />
               </IceFormBinder>
+            </Col> */}
+            <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
+              <label style={styles.filterTitle}>网关端口</label>
+              <IceFormBinder name="gwPort">
+                <Input hasClear placeholder="eg: 8080"/>
+              </IceFormBinder>
             </Col>
             <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
+              <label style={styles.filterTitle}>签名内容</label>
+              <IceFormBinder name="idiographContent">
+                <Input hasClear placeholder="eg: 【郑轻大】"/>
+              </IceFormBinder>
+            </Col>
+            <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
+              <label style={styles.filterTitle}>网关账户</label>
+              <IceFormBinder name="username">
+                <Input hasClear placeholder="eg: luan"/>
+              </IceFormBinder>
+            </Col>
+            <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
+              <label style={styles.filterTitle}>账户密码</label>
+              <IceFormBinder name="password">
+                <Input hasClear placeholder="eg: luan"/>
+              </IceFormBinder>
+            </Col>
+            <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
+              <label style={styles.filterTitle}>网络种类</label>
+              <IceFormBinder name="networkType">
+                <Input hasClear placeholder="eg: 联通/电信/移动"/>
+              </IceFormBinder>
+            </Col>
+            
+            {/* <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
               <label style={styles.filterTitle}>结束时间</label>
               <IceFormBinder
                 name="endTime"
@@ -60,33 +97,30 @@ export default class Filter extends Component {
               >
                 <DatePicker style={styles.filterTool} />
               </IceFormBinder>
-            </Col>
+            </Col> */}
+           
             <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
-              <label style={styles.filterTitle}>尺寸</label>
-              <IceFormBinder name="size">
-                <Select placeholder="请选择" style={styles.filterTool}>
-                  <Option value="small">Small</Option>
-                  <Option value="medium">Medium</Option>
-                  <Option value="large">Large</Option>
-                </Select>
+              <label style={styles.filterTitle}>网关权重</label>
+              <IceFormBinder name="weightValue">
+                <Input hasClear placeholder="eg: 8"/>
               </IceFormBinder>
             </Col>
             <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
-              <label style={styles.filterTitle}>删除状态</label>
+              <label style={styles.filterTitle}>激活状态</label>
               <IceFormBinder name="status">
                 <Select style={styles.filterTool}>
-                  <Option value="success">成功</Option>
-                  <Option value="failed">失败</Option>
+                  <Option value="Y">Y</Option>
+                  <Option value="N">N</Option>
                 </Select>
               </IceFormBinder>
             </Col>
-            <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
+            {/* <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
               <label style={styles.filterTitle}>讨论ID</label>
               <IceFormBinder name="commentId">
                 <Input />
               </IceFormBinder>
-            </Col>
-            <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
+            </Col> */}
+            {/* <Col xxs={24} xs={12} l={8} style={styles.filterCol}>
               <label style={styles.filterTitle}>置顶</label>
               <IceFormBinder name="isStick">
                 <Select placeholder="请选择" style={styles.filterTool}>
@@ -95,7 +129,7 @@ export default class Filter extends Component {
                   <Option value="not-stick">不置顶</Option>
                 </Select>
               </IceFormBinder>
-            </Col>
+            </Col> */}
           </Row>
           <div
             style={{
@@ -103,15 +137,15 @@ export default class Filter extends Component {
               marginLeft: '12px',
             }}
           >
-            <Button onClick={this.props.onReset} type="normal">
+            {/* <Button onClick={this.props.onReset} type="normal">
               重置
-            </Button>
+            </Button> */}
             <Button
               onClick={this.props.onSubmit}
               type="primary"
               style={{ marginLeft: '10px' }}
             >
-              确定
+              保  存
             </Button>
           </div>
         </div>
